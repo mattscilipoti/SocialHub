9b135602d39a6591367a1ada1c667ba8de578e20
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-08-04 12:43:44 -0400

    Complete crud functionality for hubs

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -1,2 +1,41 @@
 class HubsController < ApplicationController
+  def index
+    @hubs = Hub.all
+  end
+
+  def new
+    @hub = Hub.new
+  end
+
+  def create
+    @hub = Hub.create(hub_params)
+    redirect_to '/'
+  end
+
+  def show
+  end
+
+  def edit
+    @hub = Hub.find(params[:id])
+  end
+
+  def update
+    @hub = Hub.find(params[:id])
+    @hub.update(hub_params)
+
+    redirect_to '/'
+  end
+
+  def destroy
+    @hub = Hub.find(params[:id])
+    @hub.destroy
+
+    redirect_to '/'
+  end
+
+  private
+
+  def hub_params
+    params.require(:hub).permit(:acc_link, :photo_url, :title, :description)
+  end
 end
